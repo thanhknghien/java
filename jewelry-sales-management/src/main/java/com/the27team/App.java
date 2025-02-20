687592dbb28bd43ed8f6caef53818b0537a9aae0
thanhknghien <thanh2005dinh@gmail.com>
2025-02-20 14:14:12 +0700

    thanh

--- jewelry-sales-management/src/main/java/com/the27team/App.java
+++ jewelry-sales-management/src/main/java/com/the27team/App.java
@@ -4,10 +4,16 @@ package com.the27team;
  * Hello world!
  *
  */
+
+import javax.swing.JFrame;
 public class App 
 {
+    public JFrame jFrame1 =new JFrame();
+    
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+        System.out.print("thanh");
+        
     }
 }
