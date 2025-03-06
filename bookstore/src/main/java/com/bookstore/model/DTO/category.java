88cb653eef1564db8faf2bf9282326d487cbdcd2
hongchi <chiautowin2225@gmail.com>
2025-03-06 15:56:18 +0700

    ..

---
+++ bookstore/src/main/java/com/bookstore/model/DTO/category.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bookstore.model.DTO;
+
+/**
+ *
+ * @author 84862
+ */
+public class category {
+    private int CategoryID;
+    private String Name;
+    public category(int CategoryID, String Name){
+        this.CategoryID = CategoryID;
+        this.Name = Name;
+    }
+
+    public int getCategoryID() {
+        return CategoryID;
+    }
+
+    public void setCategoryID(int CategoryID) {
+        this.CategoryID = CategoryID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+    
+}
