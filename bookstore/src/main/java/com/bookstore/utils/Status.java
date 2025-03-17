968d6cf20340ee77e04e95d06f1af701ef90fc7d
laiPG <2kgia.lai@gmail.com>
2025-03-17 16:30:42 +0700

    lai

--- bookstore/src/main/java/com/bookstore/utils/Status.java
+++ bookstore/src/main/java/com/bookstore/utils/Status.java
@@ -2,14 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
  */
-package com.bookstore.model.DTO;
+package com.bookstore.utils;
 
 /**
  *
  * @author HP
  */
 public enum Status {
-     ACTIVE("Active"),
+    ACTIVE("Active"),
     INACTIVE("Inactive"),
     SUSPENDED("Suspended");
     private final String description;
