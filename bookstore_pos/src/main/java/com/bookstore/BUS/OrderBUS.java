1e8d295aa81511a4ec85ea51f17b3db0918a550e
thanhknghien <thanh2005dinh@gmail.com>
2025-04-05 13:17:00 +0700

    thánh

--- bookstore_pos/src/main/java/com/bookstore/BUS/OrderBUS.java
+++ bookstore_pos/src/main/java/com/bookstore/BUS/OrderBUS.java
@@ -1,5 +1,5 @@
 package com.bookstore.BUS;
 
-public class OrderService {
+public class OrderBUS {
 
 }
--- bookstore_pos/src/main/java/com/bookstore/BUS/ProductBUS.java
+++ bookstore_pos/src/main/java/com/bookstore/BUS/ProductBUS.java
@@ -1,5 +1,5 @@
 package com.bookstore.BUS;
 
-public class ProductService {
+public class ProductBUS {
 
 }
