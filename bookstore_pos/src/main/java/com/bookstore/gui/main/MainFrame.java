c85716dc768fe1b0c424686a766d244fe566467a
Hello <chi876543@gmail.com>
2025-05-08 15:35:31 +0700

    chi

---
+++ bookstore_pos/src/main/java/com/bookstore/gui/main/MainFrame.java
@@ -0,0 +1,87 @@
+package com.bookstore.gui.main;
+
+import javax.swing.*;
+import com.bookstore.gui.component.Button;
+import javax.swing.border.Border;
+
+import java.awt.*;
+public class MainFrame extends JFrame {
+    private JPanel westPanel;
+    private JPanel centerPanel;
+
+    private JPanel tkPanel;
+    private JLabel username;
+
+    private JPanel pessPanel;
+    private Button posGUI;
+    private Button categoryButton;
+    private Button customerButton;
+    private Button userButton;
+    private Button statiscalButton;
+    private Button productButton;
+
+    private Button logout;
+
+    public MainFrame() {
+        initComponents();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    private void initComponents() {
+        setSize(1500, 800);
+        setLayout(new BorderLayout());
+
+        
+        westPanel = new JPanel(new GridBagLayout()); this.add(westPanel, BorderLayout.WEST);
+        centerPanel = new JPanel(); this.add(centerPanel, BorderLayout.CENTER);
+
+        westPanel.setPreferredSize(new Dimension(300, 800));
+        centerPanel.setPreferredSize(new Dimension(1200, 800));
+
+        Border blackLine = BorderFactory.createLineBorder(Color.BLACK);
+        westPanel.setBorder(blackLine);
+
+        tkPanel = new JPanel(new GridLayout(0, 1, 10, 20)); westPanel.add(tkPanel);
+        pessPanel = new JPanel(new GridLayout(0, 1, 0, 20)); westPanel.add(pessPanel);
+        
+        tkPanel.setBorder(blackLine);
+        username = new JLabel("UserName: ");
+        logout = new Button("Đăng xuất");
+        logout.setPreferredSize(new Dimension(270, 40));
+        tkPanel.add(username);
+        tkPanel.add(logout);
+
+        posGUI = new Button("Tạo hóa đơn");
+        posGUI.setPreferredSize(new Dimension(270, 40));
+        pessPanel.add(posGUI);
+
+        categoryButton = new Button("Quản lý danh mục");
+        categoryButton.setPreferredSize(new Dimension(270, 40));
+
+        customerButton = new Button("Quản lý khách hàng");
+        customerButton.setPreferredSize(new Dimension(270, 40));
+
+        userButton = new Button("Quản lý người dùng");
+        userButton.setPreferredSize(new Dimension(270, 40));
+
+        productButton = new Button("Quản lý sản phẩm");
+        productButton.setPreferredSize(new Dimension(270, 40));
+
+        statiscalButton = new Button("Thống kê");
+        statiscalButton.setPreferredSize(new Dimension(270, 40));
+
+        pessPanel.add(categoryButton);
+        pessPanel.add(customerButton);
+        pessPanel.add(userButton);
+        pessPanel.add(productButton);
+        pessPanel.add(statiscalButton);
+
+    }   
+
+    public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+
+    }
+}
